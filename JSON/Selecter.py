1132c4a357b1f9ab3b27853426d58a4bf3b74edd
Etienne Sudre <etienne.sudre@orange.fr>
2020-06-10 15:18:01 +0200

    Gold standard stuff

--- JSON/Selecter.py
+++ JSON/Selecter.py
@@ -1,5 +1,6 @@
 import os
 lfiles = os.listdir('./Parts')
+gs = os.listdir('./GoldStandard')
 
 label = open('./labels.json', "w")
 contenulabel = '{"labels":['
@@ -8,25 +9,34 @@ classeActu = ''
 first = True
 
 for file in lfiles:
-    tab = file.split('-')
-    if len(tab) == 1:
-        classe = 'image'
-    else:
-        classe = tab[0]
-    if classe != classeActu:
-        classeActu = classe
-        if not first:
-            contenu += ']}'
-            fichier.write(contenu)
-            fichier.close()
-        first = False
-        fichier = open('./ShapePartsNames/' + classe + '.json', "w")
-        contenu = '{"shapenames":['
-        contenulabel += '"' + classe + '" ,'
+    if file not in gs:
+        tab = file.split('-')
+        if len(tab) == 1:
+            classe = 'image'
+        else:
+            classe = tab[0]
+        if classe != classeActu:
+            classeActu = classe
+            if not first:
+                contenu += ']}'
+                fichier.write(contenu)
+                fichier.close()
+            first = False
+            fichier = open('./ShapePartsNames/' + classe + '.json', "w")
+            contenu = '{"shapenames":['
+            contenulabel += '"' + classe + '" ,'
+        nomFile = file.split('.')[0]
+        contenu += '"' + nomFile + '", '
+
+fichier = open('./ShapePartsNames/gold_standard.json', "w")
+contenu = '{"shapenames":['
+for file in gs:
     nomFile = file.split('.')[0]
-    contenu += '"' + nomFile + '", '
-    
-contenulabel += ']}'
+    contenu += '"' + nomFile + '",'
+contenu += ']}'
+fichier.write(contenu)
+
+contenulabel += '"gold_standard"]}'
 label.write(contenulabel) 
 
 fichier.close()
